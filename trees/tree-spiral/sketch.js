46a99e4b09248987aa0f00ce59e5cd1ce53caeb1
Lee Biddiscombe <lee.biddiscombe@coop.co.uk>
2023-09-10 22:52:39 +0100

    Make spiral elliptical

--- trees/tree-spiral/sketch.js
+++ trees/tree-spiral/sketch.js
@@ -8,34 +8,30 @@ function setup() {
 function draw() {
   background(255);
 
-  let x = width * 0.5;
-  let r1 = 100;
-  let a = -PI / 2;
-  const da = random(0.01, 0.1);
+  const r1 = 100;
+  const r2 = 50;
   const maxy = height * 0.75;
   const trunkWidth = 16;
 
+  let x = width * 0.5;
+  let a = -PI / 2;
 
-  stroke(0, 60, 40);
-  strokeWeight(trunkWidth);
-  line(width / 2, maxy, width / 2, height);
-
-  for (let y = maxy; y >= 0; y -= 0.1) {
+  for (let y = height; y >= 0; y -= 0.1) {
     const branchChance = random(map(y, 0, height, 2, 0.2)) < 0.1;
-    const droop = map(y, 0, height, 10, 70);
     const w = map(y, 0, height, 0.1, 2);
     const dx = cos(a) * r1 * w;
+    const dy = sin(a) * r2 * w;
 
-    if (branchChance) {
+    if (branchChance & y < height * 0.75) {
       strokeWeight(map(y, 0, height, 1, trunkWidth / 2));
       stroke(140, 60, random(30, 40));
-      line(x, y, x + dx, y + droop);
+      line(x, y, x + dx, y + dy);
     }
 
     stroke(0, 60, 40);
     strokeWeight(map(y, 0, height, 1, trunkWidth));
     point(x, y);
-    a += da;
+    a += random(0.01, 0.1);
     x += random(-0.4, 0.4);
 
   }
